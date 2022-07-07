ba160d73ec196e0c9cdc1fd427b2cf6f188118c0
Guo98 <guoandy86@gmail.com>
2022-07-06 23:10:51 -0400

    initial deployment

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const helmet = require("helmet");
-// const morgan = require("morgan");
-// const addRow = require("./services/googleSheets");
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
-import helmet from "helmet";
-import morgan from "morgan";
 import { addRow } from "./services/googleSheets.js";
 // defining the Express app
 const app = express();
@@ -16,28 +7,22 @@ const app = express();
 const ads = [{ title: "Hello, world (again)!" }];
 
 // adding Helmet to enhance your Rest API's security
-app.use(helmet());
-
-// using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
-
-// enabling CORS for all requests
-app.use(cors());
-
-// adding morgan to log HTTP requests
-app.use(morgan("combined"));
-
 // defining an endpoint to return all ads
 app.get("/", (req, res) => {
   res.send(ads);
 });
 
 app.post("/offboard", async (req, res) => {
-  const resp = await addRow(req.body);
-  res.send(resp.data);
+  if (req.body && req.body !== {}) {
+    const resp = await addRow(req.body);
+    res.send(resp.data);
+  } else {
+    res.send({ message: "no body here" });
+  }
 });
 
 // starting the server
-app.listen(3001, () => {
-  console.log("listening on port 3001");
+app.listen(process.env.PORT || 3001, () => {
+  console.log("listening on port: ", process.env.PORT);
 });
