80553eb742cd930ed76073a69e1573f0825f9913
Andy Guo <andy@withspoke.com>
2023-01-16 12:51:45 -0500

    auth address

--- routes/validation.js
+++ routes/validation.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { basicAuth } from "../services/basicAuth.js";
 import { autocompleteAddress, validateAddress } from "../services/address.js";
+import { checkJwt } from "../services/auth0.js";
 
 const router = Router();
 
@@ -97,6 +98,29 @@ router.post("/validateAddress", async (req, res) => {
     } else {
       res.status(401).json({ message: "Wrong Authentication" });
     }
+  } else {
+    res.status(500).json({ message: "Missing Body" });
+  }
+});
+
+router.post("/validateAddressUI", checkJwt, async (req, res) => {
+  if (req.body && req.body !== {}) {
+    validateAddress(req.body.address)
+      .then((data) => {
+        // console.log("address data ::::: ", data);
+        if (data.status && data.status === 200) {
+          res.send({ message: "Successful!", data: data.data });
+        } else {
+          throw new Error(data.data);
+        }
+      })
+      .catch((err) => {
+        if (err.message === "not in the united states") {
+          res.status(500).json({ message: "Country not supported" });
+        } else {
+          res.status(500).json({ message: "Error validating address" });
+        }
+      });
   } else {
     res.status(500).json({ message: "Missing Body" });
   }
