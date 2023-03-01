44cce2103c5c50515774a1dea4dcac455382c51c
Andy Guo <andy@withspoke.com>
2023-02-28 21:21:23 -0500

    checkjwt

--- routes/inventory.js
+++ routes/inventory.js
@@ -202,7 +202,7 @@ router.get("/downloadinventory/:client", checkJwt, async (req, res) => {
 });
 
 // add to stock
-router.post("/addtostock", async (req, res) => {
+router.post("/addtostock", checkJwt, async (req, res) => {
   const {
     client,
     device_name,
