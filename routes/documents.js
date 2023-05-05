a615b5f4e5ce1e1ab663d26ebc538b7cf01b819c
Andy Guo <andy@withspoke.com>
2023-05-05 08:56:38 -0400

    download doc route

--- routes/documents.js
+++ routes/documents.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { DefaultAzureCredential } from "@azure/identity";
 import { BlobServiceClient } from "@azure/storage-blob";
+import { checkJwt } from "../services/auth0";
 
 const router = Router();
 
-router.get("/documents", async (req, res) => {
+router.get("/documents", checkJwt, async (req, res) => {
   let blobNames = [];
   try {
     const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
@@ -15,7 +16,7 @@ router.get("/documents", async (req, res) => {
       new DefaultAzureCredential()
     );
 
-    const containerClient = blobServiceClient.getContainerClient("image");
+    const containerClient = blobServiceClient.getContainerClient("quotes");
 
     for await (const blob of containerClient.listBlobsFlat()) {
       //const tempBlockBlobClient = containerClient.getBlockBlobClient(blob.name);
@@ -28,5 +29,37 @@ router.get("/documents", async (req, res) => {
 
   res.send({ status: "Hello World", blobNames: blobNames });
 });
+
+router.get("/downloaddoc", async (req, res) => {
+  try {
+    const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+    if (!accountName) throw new Error("Azure Storage accountName not found");
+
+    const blobServiceClient = new BlobServiceClient(
+      `https://${accountName}.blob.core.windows.net`,
+      new DefaultAzureCredential()
+    );
+
+    const containerClient = blobServiceClient.getContainerClient("quotes");
+
+    const blockBlobClient = await containerClient.getBlockBlobClient(
+      "delllaptop.jpeg"
+    );
+
+    const downloadBlockBlobResponse = await blockBlobClient.download(0);
+
+    console.log(
+      "/downloadDoc => Download document response: ",
+      downloadBlockBlobResponse
+    );
+  } catch (e) {
+    console.log(`/downloadDoc => Error in downloading document.`);
+    res.status(500).json({ status: "Error in downloading" });
+  }
+
+  if (!res.headersSent) res.send({ status: "Success" });
+});
+
+router.post("/uploadDoc", checkJwt, async (req, res) => {});
 
 export default router;
