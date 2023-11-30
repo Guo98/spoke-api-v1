6c7b935b8aa7a57b6c65334f9a5d61b2f74cc2c5
Andy Guo <andy@withspoke.com>
2023-11-29 20:21:00 -0500

    add marketplace

--- routes/marketplace.js
+++ routes/marketplace.js
@@ -36,22 +36,30 @@ router.post("/marketplace/add", checkJwt, async (req, res) => {
     ssd,
     supplier_url,
     color,
+    locations,
   } = req.body;
   console.log(`/marketplace/add/${client} => Starting function.`);
 
   try {
     console.log(`/marketplace/add/${client} => Getting all marketplace items.`);
     const marketplace = await inventory.getAll("MarketplaceInventory");
-
+    const formatted_specs = screen_size + ", " + cpu + ", " + ram + ", " + ssd;
+    let type_exists = false;
     marketplace.forEach(async (market) => {
+      let updated_marketplace = { ...market };
       if (market.client === client) {
         if (type.toLowerCase() === market.item_type.toLowerCase()) {
+          type_exists = true;
           // go thru different brands
+          let brand_exists = false;
           market.forEach(async (device_brand, brand_index) => {
             if (device_brand.brand === brand) {
               // go through different brand lines
+              brand_exists = true;
+              let line_exists = false;
               device_brand.types.forEach(async (d_t, d_t_index) => {
                 if (d_t.type.toLowerCase() === device_line.toLowerCase()) {
+                  line_exists = true;
                   // check specs to makes sure it hasn't been added already
                   d_t.specs.forEach(async (d_s) => {
                     const no_spaces_spec = d_s.spec
@@ -68,37 +76,117 @@ router.post("/marketplace/add", checkJwt, async (req, res) => {
                       );
                       res.json({ status: "Already exists" });
                     } else {
-                      let updated_marketplace = { ...market };
                       updated_marketplace.brands[brand_index].types[
                         d_t_index
                       ].specs.push({
-                        spec:
-                          screen_size + ", " + cpu + ", " + ram + ", " + ssd,
-                        locations: [],
+                        spec: formatted_specs,
+                        locations,
                         supplier: {
                           cdw: { [color]: supplier_url },
                         },
                       });
-                      try {
-                        await inventory.marketplaceUpdateSelections(
-                          updated_marketplace.id,
-                          client,
-                          updated_marketplace
-                        );
-                      } catch (err) {
-                        console.log(
-                          `/marketplace/add/${client} => Error in updating specs.`
-                        );
-                      }
                     }
                   });
                 }
               });
+
+              if (!line_exists) {
+                updated_marketplace.brands[brand_index].types.push({
+                  type: device_line,
+                  colors: [color],
+                  specs: [
+                    {
+                      spec: formatted_specs,
+                      locations,
+                      supplier: {
+                        cdw: { [color]: supplier_url },
+                      },
+                    },
+                  ],
+                });
+              }
             }
           });
+
+          if (!brand_exists) {
+            updated_marketplace.brands.push({
+              brand,
+              types: [
+                {
+                  type: device_line,
+                  colors: [color],
+                  specs: [
+                    {
+                      spec: formatted_specs,
+                      locations,
+                      supplier: {
+                        cdw: { [color]: supplier_url },
+                      },
+                    },
+                  ],
+                },
+              ],
+            });
+          }
+        }
+
+        if (JSON.stringify(market) !== JSON.stringify(updated_marketplace)) {
+          try {
+            await inventory.marketplaceUpdateSelections(
+              updated_marketplace.id,
+              client,
+              updated_marketplace
+            );
+          } catch (err) {
+            console.log(
+              `/marketplace/add/${client} => Error in updating specs.`
+            );
+          }
         }
       }
     });
+
+    if (!type_exists) {
+      const new_doc = {
+        id: type.toLowerCase() + "-" + client.toLowerCase(),
+        item_type: type,
+        client,
+        brands: [
+          {
+            brand,
+            types: [
+              {
+                type: device_line,
+                colors: [color],
+                specs: [
+                  {
+                    spec: formatted_specs,
+                    locations,
+                    supplier: {
+                      cdw: { [color]: supplier_url },
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      };
+
+      try {
+        const added_item = await inventory.addItem(
+          "MarketplaceInventory",
+          new_doc
+        );
+      } catch (e) {
+        res
+          .status(500)
+          .json({
+            status: "Error",
+            data: "Error in adding new type to marketplace",
+          });
+      }
+    }
   } catch (e) {
     console.log(
       `/marketplace/add/${client} => Error in getting all marketplace:`,
