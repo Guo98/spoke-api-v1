d399ef7881c0add5e00f01e5062efb7e0fb2301d
Andy Guo <andy@withspoke.com>
2024-01-22 22:19:20 -0500

    quick update

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,7 @@
 var config = {};
 
-config.endpoint = "https://spoketech.documents.azure.com:443/";
-config.key =
-  "YWiLZpTTCgrT0nJ8dPx4eGbdMPI0iUURw9ncu3BKYtCyGgQsHd33HLzIIefZinfK5JbeqHcjr1zOACDbke2QuQ==";
+config.endpoint = process.env.COSMOS_ENDPOINT;
+config.key = process.env.COSMOS_KEY;
 
 config.database = {
   id: "Spoke",
