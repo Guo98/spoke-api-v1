3e5be611480a192f5d8b2eb7a30c4e0891a5493c
Andy Guo <andy@withspoke.com>
2024-02-13 15:44:29 -0500

    send email fix

--- services/suppliers/cdw/order.js
+++ services/suppliers/cdw/order.js
@@ -14,6 +14,48 @@ const cdw_config = {
 
 async function placeCDWOrder(order_body) {
   console.log(`placeCDWOrder() => Starting function.`);
+  const client = new ClientCredentials(cdw_config);
+
+  try {
+    const accessToken = await client.getToken();
+    console.log(`placeCDWOrder() => Successfully got token.`);
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization:
+          accessToken.token.token_type + " " + accessToken.token.access_token,
+      },
+      url: process.env.CDW_TOKEN_HOST + "/b2b/customer/inbapi/v1/CustomerOrder",
+      data: order_body,
+    };
+
+    console.log(`placeCDWOrder() => Passing through options: `, options);
+
+    const order_resp = await axios.request(options);
+
+    if (order_resp.data?.statusCode === "RECEIVED") {
+      console.log(
+        `placeCDWOrder() => Successfully placed order: `,
+        order_resp.data
+      );
+
+      return order_resp.data.orderLines[0].CDWOrderReference;
+    } else {
+      console.log(
+        `placeCDWOrder() => No RECEIVED status in order response: `,
+        order_resp.data
+      );
+      return "";
+    }
+  } catch (e) {
+    console.log(`placeCDWOrder() => Error in getting access token: `, e);
+    return "Error";
+  }
+}
+
+async function placeCDWOrder2(order_body) {
+  console.log(`placeCDWOrder() => Starting function.`);
   //const client = new ClientCredentials(cdw_config);
 
   try {
